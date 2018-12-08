da7cd04dbe6215153e6e1b4a0e4a59be10be8cf2
Sergheusz <skymloder@gmail.com>
2018-12-08 16:00:13 -0300

    Update main.js
    EMACScript 6 syntax  & const variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,31 @@
-    var date1 = document.getElementById('date1')
-    var date2 = document.getElementById('date2')
-    var time1 = document.getElementById('time1')
-    var time2 = document.getElementById('time2')
-    var calcular = document.getElementById('calcular')
-    var diferencia = document.getElementById('resultado')
-    var seteo = moment().format('YYYY-MM-DD'); // setea fecha y formato YYYY-MM-DD
-    date1.value = seteo // setea valor de fecha actual en value del input id date1
-    date2.value = seteo // setea valor de fecha actual en value del input id date2
+const footer = document.getElementById (`footer`);
+const autor = (`Gabriel D. Hidalgo`);
+const yearcopyright = (moment().format(`YYYY`));
+const licenseGPL=(`El contenido está disponible bajo licencia GNU GPLv3.`);
+const date1 = document.getElementById(`date1`);
+const date2 = document.getElementById(`date2`);
+const time1 = document.getElementById(`time1`);
+const time2 = document.getElementById(`time2`);
+const calcular = document.getElementById(`calcular`);
+const diferencia = document.getElementById(`resultado`);
+const msgfooter = document.write(`© ${yearcopyright} ${autor} </br> ${licenseGPL}`);
 
+    
+    seteo = moment().format(`YYYY-MM-DD`); // setea fecha y formato YYYY-MM-DD
+    date1.value = (seteo); // setea valor de fecha actual en value del input id date1
+    date2.value = (seteo); // setea valor de fecha actual en value del input id date2
+    
     function negativefix(x) {
       if (x < 0) {
-        return diferencia.value = ('Ingreso una fecha de inicio no valida')
+        let MsgError = `Ingreso una fecha no valida`;
+        return diferencia.value = (MsgError);
       } else {
-        return false
+        return false;
       }
     }
     calcular.onclick = function () {
-      var now = (date1.value + ' ' + time1.value + ':00');
-      var then = (date2.value + ' ' + time2.value + ':00');
+      var now = (date1.value + ` ` + time1.value + `:00`);
+      var then = (date2.value + ` ` + time2.value + `:00`);
       var diff = moment.duration(moment(then).diff(moment(now)));
       var days = parseInt(diff.asDays());
       var hours = parseInt(diff.asHours());
@@ -27,14 +35,18 @@
       var validator = negativefix(diff)
       if (validator == false) {
         if (days <= 0 && hours > 0) {
-          diferencia.value = ('Duracion del incidente: ' + hours + ' Hs ' + minutes + ' Min')
+          let salida = `Duracion del incidente:  ${hours} Hs ${minutes} Min`;
+          diferencia.value = (salida);
         } else if (hours <= 0 && days <= 0) {
-          diferencia.value = ('Duracion del incidente: ' + minutes + ' Min')
+           let  salida= `Duracion del incidente:  ${minutes} Min`;
+           diferencia.value = (salida);
         } else {
           if (days>1){
-            diferencia.value = ('Duración del incidente: ' + days + ' Dias ' + hours + ' Hs ' + minutes + ' Min')
+            let  salida = `Duración del incidente:  ${days} Dias ${hours} Hs  ${minutes} Min`;
+            diferencia.value = (salida);
           } else{
-            diferencia.value = ('Duración del incidente: ' + days + ' Dia ' + hours + ' Hs ' + minutes + ' Min')
+              let salida = `Duración del incidente: ${days} Dia ${hours} Hs  ${minutes} Min`;
+              diferencia.value = (salida);
           }
         }
       }
